9dc4a27203c2804231560a7957a78bcd42121ad1
Karthikeyan <karthikeyan1411m@gmail.com>
2022-03-25 19:48:35 +0530

    minor change

--- 3_Implementation/game_v1.c
+++ 3_Implementation/game_v1.c
@@ -10,7 +10,7 @@ void test(){
 		float scr=0;
 		FILE *f;
 		f=fopen("score.txt","r");
-		fscanf(f,"%s%f",&name,&scr);
+		fscanf(f,"%s %f",&name,&scr);
 		printf("\n\n\t\t*************************************************************");
 		printf("\n\n\t\t %s has secured the Highest Score %f",name,scr);
 		printf("\n\n\t\t*************************************************************");
