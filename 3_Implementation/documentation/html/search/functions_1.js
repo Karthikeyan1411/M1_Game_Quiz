6f59e1ee8c1bf40849e0bb0a16bb7271e4bcb177
Karthikeyan M <64922390+Karthikeyan1411@users.noreply.github.com>
2022-03-31 19:25:02 +0530

    updated

--- 3_Implementation/documentation/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['help_0',['help',['../game__operations_8h.html#a97ee70a8770dc30d06c744b24eb2fcfc',1,'game_operations.h']]]
-];
--- 3_Implementation/documentation/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quiz_20game_20application_20by_20_22karthikeyan_20m_22_0',['Quiz Game Application by &quot;Karthikeyan M&quot;',['../index.html',1,'']]]
-];
